ee0db5befec9b616eb56eeea80399d6d7e93d06f
Adnrew <ado.ponechal@gmail.com>
2022-03-30 22:55:22 +0200

    nova verzia bez nacitania dat do premennej

--- corona.sh
+++ corona.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
+#mc tinysurvival eu
 
 env POSIXLY_CORRECT=1 | echo -n #trosku ojeb ale uvidme
 
-#BEGIN
-#funkcia na vypis pouzitia a ukoncenie corona aplikacie
+declare -a csv_files=("")
+declare -a bz2_files=("")
+declare -a gz_files=("")
+
 usage() {
   echo corona [-h] [FILTERS] [COMMAND] [LOG [LOG2 [...]]
   exit 1; #mozna popsat signal 1 - co znamena ta picovna
 }
 
-#OUTPUT
-# Pokud má skript vypsat seznam, každá položka je vypsána na jeden řádek a pouze jednou. Není-li uvedeno jinak, je pořadí řádků dáno abecedně. Položky se nesmí opakovat.
-# Pokud skript nedostane ani filtr ani příkaz, opisuje záznamy na standardní výstup.
-# Grafy jsou vykresleny pomocí ASCII a jsou otočené doprava. Hodnota řádku je vyobrazena posloupností znaku mřížky #.
-
-#FILTERS
-
-#verifying filters
 normal_year_pattern='^[0-9]{4}(\-)((((0[13578]|1[02])(\-)31))|((0[13456789]|1[012])(\-)(30|29))|((0[1-9]|1[0-2])(\-)(0[1-9]|1[0-9]|2[0-8])))$'
 leap_year_pattern='^[0-9]{2}([02468][048]|[13579][26])(\-)((((0[13578]|1[02])(\-)31))|((0[13456789]|1[012])(\-)(30))|((0[1-9]|1[0-2])(\-)(0[1-9]|1[0-9]|2[0-9])))$'
 
@@ -34,7 +29,6 @@ verify_date() {
 }
 
 age_regex='^[1-9][0-9]*$'
-
 verify_age(){
   if ! [[ $1 =~ $age_regex ]]
   then 
@@ -44,112 +38,40 @@ verify_age(){
   fi
 }
 
+#premenne pre histogram
+histogram="false" # ci bol zadany prepinac
 histogram_width=""
+max_value=1
 histogram_commands='^(gender|age|daily|monthly|yearly|countries|districts|regions)$'
 
-#PREPINAC -s
-
-#histogram_width
-#max_value 
-
-#ak je zadany prepinac s:
-
-
-#nacitaj argument a over ho
-# PO PRVE 
-    #neprazndy argument
-      #nacitaj do premennej histogram_width
-# PO DRUHE
-    #prazdny
-      #postupuj podla defaultnych hodnot (pre kazdy z histogram_commands vlastna hodnota)
-
-
-#ak neni zadany prepinac s;
- #postupuj standardnym behom programu
-
-
-# gender — 100 000
-# age — 10 000
-# daily — 500
-# monthly — 10 000
-# yearly — 100 000
-# countries — 100
-# districts — 1 000
-# regions — 10 000
-
-# VZOREC na vypocet pre grafy: 
-# WIDTH - defined:
-#  WIDTH - pocet mriezok v grafe s najvacsou hodnotou: 
-#cislo/max-width = WIDTH, kde x je najvacsie cislo => pocet# = cislo/max_value * histogram_width (zaokruhlovat dole.) 
-#PRIPAD CISLO 1. ak je sirka definovana - # = (cislo = max_value)/max_value * histogram_width = histogram_width
-#PRIPAD CISLO 2. ak je sirka nedefinovana - # 'cislo/max_value * 1 = cislo/max_value
-
 check_histogram_width() {
-  if [[ $1 =~ ^[1-9][0-9]*$ ]] #mozno dorobit overenie pre cisla jebnuteho typu 0000025 atd
+  histogram="true"
+
+  if [[ $1 =~ ^[1-9][0-9]*$ ]] 
   then 
   histogram_width=$1
   max_value="get_max_value"
   else
     if [[ $1 =~ $optstring_commands ]]
     then
-      ((OPTIND--)) 
+      ((OPTIND--))  #option index - 1 preto, aby sme nepreskocili prikaz, ak by bol za prepinacom -s rovno prikaz
     fi
   fi
 }
 
-#PRIPAD CISLO 2
-# if [[ "$histogram_width" = "" ]]
-#histogram_width=1
-# case "$command" in 
-#     "gender") 
-#          max_value=100000
-#     ;;
-#     "age")
-#          max_value=10000
-#     ;;
-#     "daily")
-#          max_value=500
-#     ;;
-#     "monthly")
-#          max_value=10000
-#     ;;
-#     "yearly")
-#          max_value=100000
-#     ;;
-#     "countries")
-#          max_value=100
-#     ;;
-#     "districts")
-#          max_value=1000
-#     ;;
-#     "regions")
-#          max_value=10000
-#     ;;
-# esac 
+#inicializacia filtrov
+after_date="0-0-0" #implicitna najnizsia hodnota - pri porovnani stringov nie je nic mesnie ako 0 (v nasom pripade pri porovnavani cisel)
+before_date="9-9-9" 
+gender=""
 
-# FILTERS může být kombinace následujících (každý maximálně jednou):
-# -a DATETIME — after: jsou uvažovány pouze záznamy PO tomto datu (včetně tohoto data). DATETIME je formátu YYYY-MM-DD.
-# -b DATETIME — before: jsou uvažovány pouze záznamy PŘED tímto datem (včetně tohoto data).
-# -g GENDER — jsou uvažovány pouze záznamy nakažených osob daného pohlaví. GENDER může být M (muži) nebo Z (ženy).
-# -s [WIDTH] u příkazů gender, age, daily, monthly, yearly, countries, districts a regions vypisuje data ne číselně, ale graficky v podobě histogramů. Nepovinný parametr WIDTH nastavuje šířku histogramů, tedy délku nejdelšího řádku, na WIDTH. Tedy, WIDTH musí být kladné celé číslo. Pokud není parametr WIDTH uveden, řídí se šířky řádků požadavky uvedenými níže.
-# (nepovinný, viz níže) -d DISTRICT_FILE — pro příkaz districts vypisuje místo LAU 1 kódu okresu jeho jméno. Mapování kódů na jména je v souboru DISTRICT_FILE
-# (nepovinný, viz níže) -r REGIONS_FILE — pro příkaz regions vypisuje místo NUTS 3 kódu kraje jeho jméno. Mapování kódů na jména je v souboru REGIONS_FILE
-# -h — vypíše nápovědu s krátkým popisem každého příkazu a přepínače.
-
-
-#PARSING FILTERS 
-
-# Skript umí zpracovat i záznamy komprimované pomocí nástrojů gzip a bzip2 (v případě, že název souboru končí .gz resp. .bz2).
-# V případě, že skript na příkazové řádce nedostane soubory se záznamy (LOG, LOG2, …), očekává záznamy na standardním vstupu.
-
+# filtre a prikazy
 optstring_filters=":a:b:g:s:h"
 optstring_commands='^(infected|merge|gender|age|daily|monthly|yearly|countries|districts|regions)$'
 head_regex='(id,datum,vek,pohlavi,kraj_nuts_kod,okres_lau_kod,nakaza_v_zahranici,nakaza_zeme_csu_kod,reportovano_khs)'
+new_line_regex='^([[:space:]]*|\r\n|\r|\n)$'
 
-#initial value of variables:
-after_date=""
-before_date=""
-gender=""
+number_of_arguments=$#
+declare -a program_arguments=("$@") 
 
 while getopts "${optstring_filters}" options; 
 do 
@@ -170,18 +92,12 @@ do
     ;;
     h) usage
     ;;
-    :) echo expected argument. 
-    ;;
     ?) echo Invalid option ; usage
     ;;
   esac
 done
 
-
-
-echo histogram width : $histogram_width
-
-
+#ziskanie stringu - ocakaveho prikazu a overenie, ci to je prikaz
 eval command=\$${OPTIND} #ziskanie nasledujuceho argumentu, ktory by mal byt command
 
 if ! [[ $command =~ $optstring_commands ]]
@@ -189,139 +105,129 @@ then
     command=""  
 fi
 
-
-echo current command: $command
-#eval file=\$$# #zatial input file na poslednom argumente
-
-#LOADING DATA
-
-#dorobit cykly pre hladanie viacerych suborov
-
-DATA=""
-
-head_regex='(id,datum,vek,pohlavi,kraj_nuts_kod,okres_lau_kod,nakaza_v_zahranici,nakaza_zeme_csu_kod,reportovano_khs)'
 head_count=0
 
-#vsetky mozne pripady ukonecnia riadka
-new_line_regex='^(\r\n|\r|\n)$'
-
-input_type="stdin"
-
-load_from_stdin() { #completed
-  while read -r line 
-  do 
-    if [[ $line =~ $head_regex ]] 
-    then 
-      DATA+="$line"
-      head_count=$(($head_count+1))
-    fi 
-
-    if (( $head_count == 1 )) && ! [[ $line =~ $head_regex ]]; then
-          DATA+=$'\n'
-          DATA+="$line"
+#nacitanie z konkretneho suboru
+read_data(){ #input type mi je na vyliz picu 
+  if [[ "$input_type" = "csv" ]];then 
+    if ((head_count == 0)); then
+        read -r head < $1
+        echo $head
+        ((head_count++))
     fi
+    if [[ "$gender" == "" ]];then
+      awk -F, -v new_line="$new_line_regex" -v before="$before_date" -v after="$after_date" '$0 !~ new_line && after <=$2 && $2<= before ' $1 
+    else
+      awk -F, -v new_line="$new_line_regex" -v before="$before_date" -v after="$after_date" -v gend="$gender" '$0 !~ new_line && after <=$2 && $2<= before && $4 == gend' $1
+    fi
+
+  else
+  if [[ "$gender" == "" ]];then 
+  if ((head_count == 0)); then
+        read -r head < $1
+        echo $head
+        ((head_count++))
+    fi
+      awk -F, -v new_line="$new_line_regex" -v before="$before_date" -v after="$after_date" '$0 !~ new_line && after <=$2 && $2<= before ' $1
+    else
+      awk -F, -v new_line="$new_line_regex" -v before="$before_date" -v after="$after_date" -v gend="$gender" '$0 !~ new_line && after <=$2 && $2<= before && $4 == gend' $1
+    fi
+  fi
+}
+
+#nacitanie zo standartneho vstupu
+read_from_stdin() { 
+  echo reading from stdin
+  while read -r line 
+        do
+        if (( $head_count == 0 )) && [[ $line =~ $head_regex ]]; then
+          echo "$line" 
+          (( head_count++ ))
+        fi
+
+        if ! [[ $line =~ $head_regex ]]; then 
+          if [[ "$gender" == "" ]];then 
+            echo "$line" | awk -F, -v new_line="$new_line_regex" -v before="$before_date" -v after="$after_date" '$0 !~ new_line && after <=$2 && $2<= before'
+          else 
+            echo "$line" | awk -F, -v new_line="$new_line_regex" -v before="$before_date" -v after="$after_date" -v gend="$gender" '$0 !~ new_line && after <=$2 && $2<= before && $4 == gend' 
+          fi
+        fi
   done
 }
 
-load_data() { #completed
- #nacitani prvej linky, kvoli hlavicke, pre kazdy typ suboru inak
-    case "$input_type" in 
-        csv) read -r line < $1 ;;
-        bz2) gzcat $1 | read -r line ;;
-        zip) unzip $1 | read -r line ;;
-        gz) gzcat $1 | read -r line ;;
-    esac 
-
-    if [[ $line =~ $head_regex ]] 
-    then 
-      head_count=$(($head_count+1))
-    fi 
-
-    if (( $head_count > 1 )) 
-    then 
-    case "$input_type" in 
-        csv) DATA+=$'\n' ; DATA+="$(awk -v header="$head_regex" -v empty_line="$new_line_regex" '{ if ($0 !~ empty_line && $0 !~ header) {print $0 } }' $1)"  ;;
-        bz2) DATA+=$'\n' ;  DATA+="$(gzcat $1 | awk -v header="$head_regex" -v empty_line="$new_line_regex" '{ if ($0 !~ empty_line && $0 !~ header) {print $0 } }' )" ;;
-        zip) DATA+=$'\n' ;  DATA+="$(unzip $1 |  awk -v header="$head_regex" -v empty_line="$new_line_regex" '{ if ($0 !~ empty_line && $0 !~ header) {print $0 } }' )" ;;
-        gz) DATA+=$'\n' ;  DATA+="$(gzcat $1 |  awk -v header="$head_regex" -v empty_line="$new_line_regex" '{ if ($0 !~ empty_line && $0 !~ header) {print $0 } }' )" ;;
-    esac 
-     #tu treba dat date a age verifycation 
-    else
-     case "$input_type" in 
-        csv) DATA+="$(awk -v empty_line="$new_line_regex" '{ if($0 !~ empty_line) {print $0}}' $1)" ;;
-        bz2)  DATA+="$(gzcat $1 |  awk -v empty_line="$new_line_regex" '{ if($0 !~ empty_line) {print $0}}' )" ;;
-        zip) DATA+="$(unzip $1 |  awk -v empty_line="$new_line_regex" '{ if($0 !~ empty_line) {print $0}}' )" ;;
-        gz) DATA+="$(gzcat $1 |  awk -v empty_line="$new_line_regex" '{ if($0 !~ empty_line) {print $0}}' )" ;;
-    esac 
-    fi
-}
-
-
- #TOTO A TERAZ DO PICII dorobit presmerovanie pre standardny vstup
-
-#tu ziskat typ suboru a podla toho to nacitat
-index=0
-while (( $index != $#+1 ))
-do
-  eval file=\$$index
-  if [[ $file =~ ^.*\.csv$ ]] 
-  then input_type="csv" ; load_data $file  
-  elif [[ $file =~ ^.*\.bz2$ ]]
-  then input_type="bz2" ; load_data $file 
-  elif [[ $file =~ ^.*\.zip$ ]] 
-  then input_type="zip" ;  load_data $file 
-  elif [[ $file =~ ^.*\.gz$ ]]
-  then input_type="gz" ; load_data $file 
-  fi 
-  ((index++))
-done 
-
-if [[ $input_type == "stdin" ]]
-then
-    load_from_stdin 
-    DATA="$(echo "$DATA" | awk  -v empty_line="$new_line_regex" '{ if ($0 !~ empty_line ) {print $0 } }' )"
-fi
-
-echo input type : $input_type
-
-#echo "$DATA"
-
 #COMMANDS
 
-# COMMAND může být jeden z:
-# infected — spočítá počet nakažených.
-# merge — sloučí několik souborů se záznamy do jednoho, zachovávající původní pořadí (hlavička bude ve výstupu jen jednou).
-# gender — vypíše počet nakažených pro jednotlivá pohlaví.
-# age — vypíše statistiku počtu nakažených osob dle věku (bližší popis je níže).
-# daily — vypíše statistiku nakažených osob pro jednotlivé dny.
-# monthly — vypíše statistiku nakažených osob pro jednotlivé měsíce.
-# yearly — vypíše statistiku nakažených osob pro jednotlivé roky.
-# countries — vypíše statistiku nakažených osob pro jednotlivé země nákazy (bez ČR, tj. kódu CZ).
-# districts — vypíše statistiku nakažených osob pro jednotlivé okresy.
-# regions — vypíše statistiku nakažených osob pro jednotlivé kraje.
-
-
 infected() { #completed
-  infected_data=$(echo "$DATA" | awk '$0 !~ /^(\r\n|\r|\n)$/' | wc -l)
-  number_infected=$(($infected_data-1)) # minus header
-  echo Infected: $number_infected
+  number_infected=0
+    #csv
+    for csv in "${csv_files[@]}";do
+    if [[ "$gender" == "" ]];then 
+        (( number_infected+=$(awk -F, -v new_line="$new_line_regex" -v head="$head_regex" -v before="$before_date" -v after="$after_date"\
+        '$0 !~ new_line && $0 !~ head && after <= $2 && $2 <= before' $csv | wc -l ) ))
+    else 
+        (( number_infected+=$(awk -F, -v new_line="$new_line_regex" -v head="$head_regex" -v before="$before_date" -v after="$after_date" -v gend="$gender"\
+        '$0 !~ new_line && $0 !~ head && after <=$2 && $2<= before && $4 == gend' $csv | wc -l ) ))
+    fi
+    
+    done
+    
+    #bz
+    for bzf in "${bz2_files[@]}"; do
+      if [[ "$gender" == "" ]];then 
+          (( number_infected+=$(gzcat -q $bzf |awk -F, -v new_line="$new_line_regex" -v head="$head_regex" -v before="$before_date" -v after="$after_date"\
+          '$0 !~ new_line && $0 !~ head && after <= $2 && $2 <= before'  | wc -l ) ))
+      else 
+          (( number_infected+=$(gzcat -q $bzf |awk -F, -v new_line="$new_line_regex" -v head="$head_regex" -v before="$before_date" -v after="$after_date" -v gend="$gender"\
+          '$0 !~ new_line && $0 !~ head && after <=$2 && $2<= before && $4 == gend'  | wc -l ) ))
+      fi
+    done
+    
+    #gzf
+    for gzf in  "${gz_files[@]}"; do
+    if [[ "$gender" == "" ]];then 
+        (( number_infected+=$(gzcat -q $gzf |awk -F, -v new_line="$new_line_regex" -v head="$head_regex" -v before="$before_date" -v after="$after_date"\
+        '$0 !~ new_line && $0 !~ head && after <= $2 && $2 <= before'  | wc -l ) ))
+    else 
+        (( number_infected+=$(gzcat -q $gzf |awk -F, -v new_line="$new_line_regex" -v head="$head_regex" -v before="$before_date" -v after="$after_date" -v gend="$gender"\
+        '$0 !~ new_line && $0 !~ head && after <=$2 && $2<= before && $4 == gend'  | wc -l ) ))
+    fi
+    done
+  echo $number_infected
 }
 
-merge() { #completed
-  echo "$DATA"
-}
+# merge() { 
+#    read_data 
+# }
 
-gender() { #completed
+gender() { 
+men=0
+women=0
+for csv in "${csv_files[@]}";do
+      (( men+=$( awk -F, -v before="$before_date" -v after="$after_date" '$4 == "M" && after <= $2 && $2 <= before'  $csv | wc -l) ))
+      (( women+=$( awk -F, -v before="$before_date" -v after="$after_date" '$4 == "Z" && after <= $2 && $2 <= before' $csv | wc -l) ))
+    done
+    for bzf in "${bz2_files[@]}"; do
+      (( men+=$( gzcat -q $bzf | awk -F, '$4 == "M"' | wc -l) ))
+      (( women+=$( gzcat -q $bzf | awk -F, '$4 == "Z"' | wc -l) ))
+    done
+
+    for gzf in  "${gz_files[@]}"; do
+      (( men+=$( gzcat -q $gzf | awk -F, '$4 == "M"'   | wc -l) ))
+      (( women+=$( gzcat -q $gzf | awk -F, '$4 == "Z"'  | wc -l) ))
+    done  
+
 case "$gender" in 
-  M) echo M: $(echo "$DATA" | awk -F, '$4 == "M"'  | wc -l) ;;
-  Z) echo Z: $(echo "$DATA" | awk -F, '$4 == "Z"' | wc -l) ;;
-  "") echo M: $(echo "$DATA" | awk -F, '$4 == "M"'  | wc -l) ; echo Z: $(echo "$DATA" | awk -F, '$4 == "Z"' | wc -l) ;;
+  M) echo M: $men ;;
+  Z) echo Z: $women ;;
+  "")  echo M: $men ; echo Z: $women ;;
+   #$( awk -F, '$4 == "M"'  $csv | wc -l) ; echo Z: $( awk -F, '$4 == "Z"' $csv | wc -l) ;;
 esac
+#if [[ "$histogram" == "true" ]]
 }
 
 age() { #dorobit zarovnanie
   #podla intervalov vypise statistiku (tabulka)
-  echo "$DATA" | awk -F, -v count0_5=0 -v count6_15=0 -v count16_25=0 -v count26_35=0 -v count36_45=0 -v count46_55=0 -v count56_65=0 -v\
+  awk -F, -v count0_5=0 -v count6_15=0 -v count16_25=0 -v count26_35=0 -v count36_45=0 -v count46_55=0 -v count56_65=0 -v\
   count66_75=0 -v count76_85=0 -v count86_95=0 -v count96_105=0 -v above_105=0 \
   'NR ==1 {next}\
   0 <= $3 && $3 <= 5 {count0_5++ } \
@@ -337,7 +243,7 @@ age() { #dorobit zarovnanie
   96 <= $3 && $3 <= 105 {count96_105++ }\
   105 < $3 {above_105++ }
 
-END{
+END{ 
 printf("\
 0-5 : %d\n\
 6-15 : %d\n\
@@ -353,13 +259,12 @@ printf("\
 > 105 : %d\n",
     count0_5 , count6_15 , count16_25 , count26_35 , count36_45, count46_55 , count56_65 , count66_75\
     , count76_85 , count86_95 , count96_105 , above_105)
-  }'
+  }' $INPUT #dorobit formatovanie
+#if [[ "$histogram" == "true" ]]
 }
 
 daily() {
-  #pozn netreba overovat datujmy - uz su overene v tomto bode
-  #date1: data 
-  #date2: data
+  #ulozit jeden datum do pola a vnimat zmeny tadaa
   #prist na sposob, ako vyfiltrovat vsetky datumy
   echo daily... #statistika podla dni
   #asi regexp 
@@ -385,6 +290,35 @@ regions() {
   echo regions... #pre kraje
 }
 
+
+# awk -F, -v before="$before_date" -v after="$after_date" -v gend="gender" 'after <=$2 && $2<= before && $4 == gend' $1
+
+input_type="stdin"
+index=0
+csv_idx=0
+bz_idx=0
+gz_idx=0
+while (( $index != $#+1 ))
+do
+  eval file=\$$index
+  if [[ $file =~ ^.*\.csv$ ]] 
+  then input_type="csv" ; csv_files[$csv_idx]="$file" ; ((csv_idx++))
+  elif [[ $file =~ ^.*\.bz2$ ]]
+  then input_type="bz2" ; bz2_files[$bz_idx]="$file" ; echo  ${bz2_files[$bz_idx]} ; ((bz_idx++))
+  elif [[ $file =~ ^.*\.gz$ ]]
+  then input_type="gz" ; gz_files[$gz_idx]="$file" ; ((gz_idx++))
+  fi
+  if (( $index == $# )) && [[ $input_type == "stdin" ]];then
+      read_from_stdin 
+  fi
+  ((index++))
+done 
+if [[ $input_type == "stdin" ]]
+then 
+  awk  -v empty_line="$new_line_regex" '{ if ($0 !~ empty_line ) {print $0 } }' 
+fi
+
+
 case "$command" in 
     infected) infected ;;
     merge) merge ;;
@@ -396,30 +330,14 @@ case "$command" in
     countries) countries ;;
     districts) districts ;;
     regions) regions ;;
-    "") merge ;;
+    "") merge  ;;
 esac
 
-#overenie datumov 
-echo "$DATA" | awk -F, -v norm_date="$normal_year_pattern" -v leap_date="$leap_year_pattern" -v age=$age_regex 'NR == 1{next}\
-( $2 !~ norm_date && $2 !~ leap_date) {printf("Invalid date: %s\n",$0)| "cat 1>&2"}\
-($3 !~ age && $3 != "") {printf("Invalid age: %s\n", $0) | "cat 1>&2"}' 
+# awk -F, -v norm_date="$normal_year_pattern" -v leap_date="$leap_year_pattern" -v age=$age_regex 'NR == 1{next}\
+# ( $2 !~ norm_date && $2 !~ leap_date) {printf("Invalid date: %s\n",$0)| "cat 1>&2"}\
+# ($3 !~ age && $3 != "") {printf("Invalid age: %s\n", $0) | "cat 1>&2"}'   #este vyriesit kde a ako to vypisat
 
-#NEXT
-#dokocint filter s 
-#zakomponovat filtre do commnadov
-#dokoncit commandy
-#bud upravit funkciu load data alebo spravit dalsie dve funkcie - pre kazdy subor jeden
+exit 0;
 
-#load your data based on input type here 
+#NAPAD ZA MILION - dat si premennu INPUT alebo pole INPUTFILES a z toho vytahovat data 
 
-#napad 1: unzip vsetky zazipovane veci a potom s nimi narabat ako s csv fileom
-
-#AKO BY TO MALO PRACOVAT
-# najprv nacitat vsetky filtre, overit ich spravnost parametrov
-# nacitat prikazy, v pripade nezmyslov pokracovat standarne ako bez prikazu alebo ukoncit program, to este nevim
-# po nacitani prikazov nacitat vstupy - stdin, subor, taky, taky, onaky
-# nakoniec vyfiltrovat potrebne zanamy pomocou potrebnych prikazov (ako awk, grep, sed a podobne)
-# vypisat output 
-# ende do pice 
-
-# overenia na stdout 
